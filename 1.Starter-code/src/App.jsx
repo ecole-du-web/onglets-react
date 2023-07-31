516184af5d40e329a7f3ed52e10e470f0a78f9b8
École du Web <ledesignerduweb@outlook.fr>
2023-07-31 16:28:52 +0200

    add starter/source

---
+++ 1.Starter-code/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="mt-24 px-6">
+
+    </div>
+  )
+}
+
+export default App
---
+++ 2.Source-code/src/data/tabs.js
@@ -0,0 +1,35 @@
+const tabsData1 = [
+  {
+    buttonContent: "Basique 🥉",
+    tabHeading: "Plan basique à 9.99€/m",
+    txt: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius quam adipisci saepe sit reiciendis sint voluptatibus fuga officiis quod eaque.",
+  },
+  {
+    buttonContent: "Pro 🥈",
+    tabHeading: "Plan professionnel à 29.99€/m",
+    txt: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio provident voluptatem ipsam exercitationem doloribus! Totam ab nihil ipsum voluptas aperiam beatae expedita quae facilis ducimus. Asperiores officia, suscipit accusantium nisi nam modi, rerum nostrum esse enim earum dolor architecto quos?",
+  },
+  {
+    buttonContent: "Entreprise 🥇",
+    tabHeading: "Plan entreprise - nous contacter",
+    txt: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio provident voluptatem ipsam exercitationem doloribus! Totam ab nihil ipsum voluptas aperiam beatae expedita quae facilis ducimus.",
+  },
+]
+const tabsData2 = [
+  {
+    buttonContent: "Chat",
+    tabHeading: "Plan basique à 9.99€/m",
+    txt: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius quam adipisci saepe sit reiciendis sint voluptatibus fuga officiis quod eaque.",
+  },
+  {
+    buttonContent: "Chien",
+    tabHeading: "Plan professionnel à 29.99€/m",
+    txt: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio provident voluptatem ipsam exercitationem doloribus! Totam ab nihil ipsum voluptas aperiam beatae expedita quae facilis ducimus. Asperiores officia, suscipit accusantium nisi nam modi, rerum nostrum esse enim earum dolor architecto quos?",
+  },
+  {
+    buttonContent: "Poisson",
+    tabHeading: "Plan entreprise - nous contacter",
+    txt: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio provident voluptatem ipsam exercitationem doloribus! Totam ab nihil ipsum voluptas aperiam beatae expedita quae facilis ducimus.",
+  },
+]
+export { tabsData1, tabsData2 }
